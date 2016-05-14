9e3988729e363af539f9e249b9ac7fb41d120704
wertrain <wertrain@gmail.com>
2016-05-14 11:16:29 +0900

    WIP

--- TgaDecoder.cs
+++ TgaDecoder.cs
@@ -6,53 +6,101 @@ namespace TgaDecoderTest
 {
     public class TgaDecoder
     {
+        protected class TgaData
+        {
+            private const int TgaHeaderSize = 18;
+            private int idFieldLength;
+            private int colorMapType;
+            private int imageType;
+            private int colorMapIndex;
+            private int colorMapLength;
+            private int colorMapDepth;
+            private int imageOriginX;
+            private int imageOriginY;
+            private int imageWidth;
+            private int imageHeight;
+            private int bitPerPixel;
+            private int descriptor;
+            private byte [] colorData;
+
+            public TgaData(byte[] image)
+            {
+                this.idFieldLength = image[0];
+                this.colorMapType = image[1];
+                this.imageType = image[2];
+                this.colorMapIndex = image[4] << 8 | image[3];
+                this.colorMapLength = image[6] << 8 | image[5];
+                this.colorMapDepth = image[7];
+                this.imageOriginX = image[9] << 8 | image[8];
+                this.imageOriginY = image[11] << 8 | image[10];
+                this.imageWidth = image[13] << 8 | image[12];
+                this.imageHeight = image[15] << 8 | image[14];
+                this.bitPerPixel = image[16];
+                this.descriptor = image[17];
+                this.colorData = new byte[image.Length - TgaHeaderSize];
+                image.CopyTo(this.colorData, TgaHeaderSize);
+            }
+
+            public int Width
+            {
+                get { return this.imageWidth; }
+            }
+
+            public int Height
+            {
+                get { return this.imageHeight; }
+            }
+
+            public int GetPixel(int x, int y)
+            {
+                if (colorMapType == 0)
+                {
+                    return 0;
+                }
+                else
+                {
+                    int colormapDataSize = this.bitPerPixel / 8 * this.colorMapLength;
+                    int imageDataOffset = colormapDataSize;
+
+                    int offset = (this.colorData[imageDataOffset] & 0xFF) - this.colorMapIndex;
+
+                    int index = 4 * offset;
+                    int b = this.colorData[index + 0] & 0xFF;
+                    int g = this.colorData[index + 1] & 0xFF;
+                    int r = this.colorData[index + 2] & 0xFF;
+                    int a = this.colorData[index + 3] & 0xFF;
+
+                    return (a << 24) | (r << 16) | (g << 8) | b;
+                }
+            }
+        }
+
         public static Bitmap FromFile(string path)
         {
-            using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+            try
             {
-                try
+                using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read))
                 {
                     int length = (int)fs.Length;
                     byte[] buffer = new byte[length];
                     fs.Read(buffer, 0, length);
                     return decode(buffer);
                 }
-                catch (Exception)
-                {
-                    return null;
-                }
+            }
+            catch (Exception)
+            {
+                return null;
             }
         }
 
         protected static Bitmap decode(byte[] image)
         {
-            // ヘッダーファイルを展開
-            int idFieldLength = image[0];
-            int colorMapType = image[1];
-            int imageType = image[2];
-            switch (imageType)
-            {
-                case  0: break; // イメージなし
-                case  1: break; // インデックスカラー（256色）
-                case  2: break; // フルカラー
-                case  3: break; // 白黒
-                case  9: break; // インデックスカラー RLE圧縮
-                case 10: break; // フルカラー RLE圧縮
-                case 11: break; // 白黒 RLE圧縮
-            }
-            int colorMapIndex = image[4] << 8 | image[3];
-            int colorMapLength = image[6] << 8 | image[5];
-            int colorMapSize = image[7];
-            int imageOriginX = image[9] << 8 | image[8];
-            int imageOriginY = image[11] << 8 | image[10];
-            int imageWidth = image[13] << 8 | image[12];
-            int imageHeight = image[15] << 8 | image[14];
-            int bitPerPixel = image[16];
-            int descriptor = image[17];
+            TgaData tga = new TgaData(image);
 
-            Bitmap bitmap = new Bitmap(imageWidth, imageHeight);
-            bitmap.SetPixel();
+            Bitmap bitmap = new Bitmap(tga.Width, tga.Height);
+            //bitmap.SetPixel();
             return bitmap;
         }
+
     }
 }
